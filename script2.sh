5efaef89633e4f123c9eeb01c0d06e1a963a352a
joshuascottpaul <joshua@neocodesoftware.com>
2021-12-22 12:57:01 -0800

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://downloads.claris.com/esd/fms_19.3.2.203.zip
+wget https://downloads.claris.com/esd/fms_19.4.2.204.zip
 
-unzip fms_fms_19.3.2.203.zip
+unzip fms_19.4.2.204.zip
 
-sudo apt install ./filemaker-server_19.3.2.203_amd64.deb -y
+sudo apt install ./filemaker-server_19.4.2.204_amd64.deb -y
 
 ps -e | grep -e fm -e apache -e fac

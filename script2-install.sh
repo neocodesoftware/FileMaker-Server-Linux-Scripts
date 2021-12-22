8a1ce5a02a2919a2a13fe640642544ced12d457b
joshuascottpaul <joshua@neocodesoftware.com>
2021-12-22 15:38:29 -0800

    Update script2-install.sh

--- script2-install.sh
+++ script2-install.sh
@@ -9,3 +9,6 @@ sudo apt install ./filemaker-server_19.4.2.204_amd64.deb -y
 sudo rm *.rtf
 
 sudo rm *.txt
+
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok   
+       

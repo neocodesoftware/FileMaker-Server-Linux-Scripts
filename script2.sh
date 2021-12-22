812c1721260420175663d5901de6691333a58269
joshuascottpaul <joshua@neocodesoftware.com>
2021-12-22 13:08:20 -0800

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -7,3 +7,5 @@ unzip fms_19.4.2.204.zip
 sudo apt install ./filemaker-server_19.4.2.204_amd64.deb -y
 
 ps -e | grep -e fm -e apache -e fac
+
+sudo service fmshelper stop ; sudo service fmshelper start

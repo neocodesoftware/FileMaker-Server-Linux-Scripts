3fd8d56ce0489c28be0dd28df2108500dddc9935
joshuascottpaul <joshua@neocodesoftware.com>
2021-12-22 13:07:10 -0800

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -3,4 +3,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install htop
+sudo apt install restic
+sudo apt install silversearcher-ag
+sudo apt install openssh-server
 lsb_release -a

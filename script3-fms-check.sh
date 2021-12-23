e7e222bfd28ea22dd4ebcbd61604e488e4d7f696
joshuascottpaul <joshua@neocodesoftware.com>
2021-12-22 16:52:56 -0800

    Update script3-fms-check.sh

--- script3-fms-check.sh
+++ script3-fms-check.sh
@@ -2,17 +2,21 @@
 #sudo service fmshelper stop ; sudo service fmshelper start
 
 echo BACKUPTIME
+echo ----------
 fmsadmin GET BACKUPTIME
 echo
 
 echo SERVERCONFIG
+echo ------------
 fmsadmin GET SERVERCONFIG
 echo
 
 echo filemaker processes
+echo -------------------
 ps -e | grep -e fm -e apache -e fac
 echo
 
 # what port filemaker admin console is using
 echo admin console port
+echo ------------------
 sudo netstat -tnlp | grep facstart
